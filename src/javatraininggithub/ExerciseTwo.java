6eef9699d8c6e1bab7c08adb0be33d304b526286
Colin Cousins <cacousins@gmail.com>
2019-08-16 20:55:55 -0400

    Java Exercise Two

---
+++ src/javatraininggithub/ExerciseTwo.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javatraininggithub;
+
+/**
+ *
+ * @author colin
+ */
+public class ExerciseTwo {
+    public static void main(String[] args) {
+        int number1 = 20;
+        int number2 = 30;
+        int sum = number1 + number2;
+        
+        System.out.println("The total of " + number1 + " and " + number2
+        + " is " + sum);
+        
+        System.out.println("The sum of 5 and 2 is " + (5+2));
+    }
+    
+}

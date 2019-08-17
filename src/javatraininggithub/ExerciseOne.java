a3fbd5365fe29ab70b0a93c2942a91f71cbe7d6e
Colin Cousins <cacousins@gmail.com>
2019-08-16 20:37:56 -0400

    Java Exercise One

---
+++ src/javatraininggithub/ExerciseOne.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javatraininggithub;
+
+/**
+ *
+ * @author colin
+ */
+public class ExerciseOne {
+    public static void main(String[] args) {
+        System.out.println("This is a test one");
+    }
+    
+}
